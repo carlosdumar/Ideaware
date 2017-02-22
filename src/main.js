3ca0252a9e23136f55925fe48befffc41431b2e3
Carlos Andres Rodriguez <cdumar9@hotmail.com>
2017-02-21 21:41:34 -0500

    Se agregan los elementos del nav

---
+++ src/main.js
@@ -0,0 +1,4 @@
+var loadCSS = require('./javascript/lib/loadCSS');
+
+loadCSS('https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans');
+loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
