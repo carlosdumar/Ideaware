7c9418a79f5becac27ab6b5cc297be7dcf3728bb
Carlos Andres Rodriguez <cdumar9@hotmail.com>
2017-02-22 10:50:10 -0500

    Se crea el footer

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 var loadCSS = require('./javascript/lib/loadCSS');
+require('./javascript/lib/navbar-menu.js');
 
 loadCSS('https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans');
 loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
